f925ea19a850df50efe74980d17751e15470dd3f
wanglei <wanglei@koolyun.com>
2018-03-09 15:13:34 +0800

    修改包名

---
+++ src/ch1/sec04/MethodReferences.java
@@ -0,0 +1,23 @@
+package ch1.sec04;
+
+import java.util.*;
+
+import ch1.sec06.Application;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+
+public class MethodReferences extends Application {
+   public void start(Stage stage) {
+      String[] strings = "Mary had a little lamb".split(" ");
+      Arrays.sort(strings, String::compareToIgnoreCase);
+      System.out.println(Arrays.toString(strings));
+
+      Button button = new Button("Click me!");
+      button.setOnAction(System.out::println);
+      stage.setScene(new Scene(button));
+      stage.show();
+   }
+}
